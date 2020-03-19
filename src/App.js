63327b2a89c2012fa2e49cc0482fa68a3c85b0da
Saša Fazlić <sasa.fazlic@burza.hr>
2020-03-19 15:39:10 +0100

    working on json data processor

--- src/App.js
+++ src/App.js
@@ -1,36 +1,41 @@
-import React from 'react';
+import React from 'react'
 import CKEditor from '@ckeditor/ckeditor5-react'
- import ClassicEditor from '@ckeditor/ckeditor5-build-classic'
-// import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
-// import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials'
-// import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold'
-// import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
-// import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph'
-// import SimpleUploadAdapter from '@ckeditor/ckeditor5-upload/src/adapters/simpleuploadadapter'
-import './App.css';
+import JsonClassicEditor, { config } from './jsonDataProcessor'
+import './App.css'
 
+const initialData = JSON.stringify([
+  {
+    name: 'ul',
+    text: 'hello',
+    children: [
+      {
+        name: 'li',
+        children: [
+          {
+            text: 'foo'
+          }
+        ]
+      }
+    ]
+  }
+])
 
+const App = () => {
+  let Ckeditor
 
-
-const App =()=>{
-let Ckeditor
-
-
-
-console.log(Ckeditor)
   return (
     <div className='App'>
       <h2>Using CKEditor 5 build in React</h2>
       <CKEditor
-        editor={ClassicEditor}
-        
-        data='<p>Hello from CKEditor 5!</p>'
+        editor={JsonClassicEditor}
+        config={config}
+        data={initialData} //'<p>Hello from CKEditor 5!</p>'
         onInit={editor => {
-          Ckeditor= editor
+          Ckeditor = editor
           editor.conversion.for('dataDowncast').add(dispatcher => {
-console.log("dispatcher",dispatcher)  
-return {sasa:2}
-})
+            console.log('dispatcher', dispatcher)
+            return { sasa: 2 }
+          })
           // You can store the "editor" and use when it is needed.
           console.log('Editor is ready to use!', editor)
         }}
@@ -45,9 +50,9 @@ return {sasa:2}
           console.log('Focus.', editor)
         }}
       />
-      <button onClick= {()=>console.log(Ckeditor.getData())}>Click me</button>
+      <button onClick={() => console.log(Ckeditor.getData())}>Click me</button>
     </div>
   )
 }
 
-export default App;
+export default App
---
+++ src/jsonDataProcessor.js
@@ -0,0 +1,111 @@
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor'
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials'
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph'
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold'
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic'
+import List from '@ckeditor/ckeditor5-list/src/list'
+import Heading from '@ckeditor/ckeditor5-heading/src/heading'
+import ViewText from '@ckeditor/ckeditor5-engine/src/view/text'
+import ViewElement from '@ckeditor/ckeditor5-engine/src/view/element'
+import ViewDocumentFragment from '@ckeditor/ckeditor5-engine/src/view/documentfragment'
+
+export const config = {
+  plugins: [Essentials, Paragraph, Bold, Italic, List, Heading]
+}
+
+export default class JsonClassicEditor extends ClassicEditor {
+  constructor(element, config) {
+    super(element, config)
+
+    this.data.processor = new JsonDataProcessor()
+  }
+}
+
+class JsonDataProcessor {
+  toData(viewFragment) {
+    console.log('data', viewFragment)
+    const json = []
+    console.log('jsonj', json)
+    for (const child of viewFragment) {
+      const childJson = viewToJson(child)
+
+      json.push(childJson)
+    }
+
+    return JSON.stringify(json)
+  }
+
+  toView(jsonString) {
+    console.log('view', jsonString)
+    const jsonData = JSON.parse(jsonString)
+    const viewFragment = new ViewDocumentFragment()
+    console.log('View Fragmen', viewFragment)
+    for (const childJson of jsonData) {
+      const child = jsonToView(childJson)
+
+      viewFragment.appendChildren(child)
+    }
+
+    return viewFragment
+  }
+}
+
+function viewToJson(viewElement) {
+  const json = {}
+
+  if (viewElement.is('text')) {
+    json.text = viewElement.data
+  } else {
+    json.name = viewElement.name
+    json.attributes = {}
+
+    for (const [key, value] of viewElement.getAttributes()) {
+      json.attributes[key] = value
+    }
+
+    json.children = []
+
+    for (const child of viewElement.getChildren()) {
+      json.children.push(viewToJson(child))
+    }
+  }
+
+  return json
+}
+
+function jsonToView(jsonObject) {
+  if (jsonObject.text) {
+    return new ViewText(jsonObject.text)
+  } else {
+    const viewElement = new ViewElement(jsonObject.name, jsonObject.attributes)
+    console.log('viewEL', viewElement)
+
+    for (const childJson of jsonObject.children) {
+      const viewChild = jsonToView(childJson)
+      viewElement.appendChildren(viewChild)
+    }
+
+    return viewElement
+  }
+}
+
+// JsonClassicEditor.create(document.querySelector('#editor'), {
+//   plugins: [Essentials, Paragraph, Bold, Italic, List, Heading],
+//   toolbar: [
+//     'headings',
+//     'bold',
+//     'italic',
+//     'bulletedList',
+//     'numberedList',
+//     'undo',
+//     'redo'
+//   ]
+// }).then(editor => {
+//   window.editor = editor
+// })
+
+// function getEditorData() {
+//   window.alert(window.editor.getData())
+// }
+
+//document.getElementById('getDataBtn').onclick = getEditorData
